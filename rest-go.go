85555ee8ca2ce0a6f8513c04dc0a449c77135e18
Gustavo Bodra <gbodra@Gustavos-MacBook-Pro.local>
2019-06-20 11:47:36 -0300

    API rest para calcular sequencia de fibonacci

--- rest-go.go
+++ rest-go.go
@@ -1,15 +1,33 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	// "fmt"
+	// "time"
+    "log"
+    "net/http"
+	"github.com/gorilla/mux"
+	"math/big"
 )
 
+func GetFibonacci(w http.ResponseWriter, r *http.Request) {
+	// initTime := time.Now().Nanosecond()
+
+	n:= uint(10000)
+	
+	var n2, n1 = big.NewInt(0), big.NewInt(1)
+
+	for i := uint(1); i < n; i++ {
+		n2.Add(n2, n1)
+		n1, n2 = n2, n1
+	}
+
+	// endTime := time.Now().Nanosecond()
+	// fmt.Println("Time (nanosec): ", (endTime - initTime))
+}
+
 func main() {
-	initTime := time.Now().Nanosecond()
+	router := mux.NewRouter()
+	router.HandleFunc("/fibonacci", GetFibonacci).Methods("GET")
 
-	fmt.Println("Hello, world.")
-
-	endTime := time.Now().Nanosecond()
-	fmt.Println("Time (nanosec): ", (endTime - initTime))
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
